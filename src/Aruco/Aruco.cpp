e3ec5f7bde306dc7919d5603f1dbb4ff04c707ca
subo <gamedevsubo@gmail.com>
2022-09-16 14:24:01 +0200

    Add Emil's changes

--- src/Aruco/Aruco.cpp
+++ src/Aruco/Aruco.cpp
@@ -1,8 +1,24 @@
 
 #include "Aruco.h"
 #include "ArucoDictionary.h"
+#include <functional>
 
 void Aruco::generate(uint16_t id, Color* buffer, uint8_t scale){
+
+	uint16_t w = 7*scale;
+	uint16_t h = 7*scale;
+	auto drawPix = [&buffer, &w, &h](uint16_t x, uint16_t y, Color c){
+		buffer[x+y*w] = c;
+	};
+
+	auto fillRect = [&buffer, &w, &h, &drawPix](uint16_t _x, uint16_t _y, uint16_t rectW, uint16_t rectH, Color c){
+		for(int y = 0; y < rectH; y++){
+			for(int x = 0; x < rectW; x++){
+				drawPix(x + _x, y + _y, c);
+			}
+		}
+	};
+
 	const uint8_t* bytes = DICTIONARY[id];
 	uint8_t bitsCount = side * side;
 	uint8_t bits[bitsCount];
