11ff430af61857208e602232490167de73c760f5
Emil Gajšak <emil.gajsak@gmail.com>
2022-09-23 17:20:19 +0200

    Added FeedService ack replying

--- src/FeedService.cpp
+++ src/FeedService.cpp
@@ -5,6 +5,7 @@
 
 const uint8_t frameStart[FRAME_LEN] = { 0x18, 0x20, 0x55, 0xf2, 0x5a, 0xc0, 0x4d, 0xaa };
 const uint8_t frameEnd[FRAME_LEN] = { 0x42, 0x2c, 0xd9, 0xe3, 0xff, 0xa0, 0x11, 0x01 };
+const uint8_t frameAck[FRAME_LEN] = { 0xba, 0x14, 0x05, 0xb0, 0x21, 0xa2, 0x03, 0x83 };
 
 FeedService Feed;
 
@@ -15,22 +16,42 @@ bool FeedService::connect(){
 	client.onConnect([this](void*, AsyncClient* server){
 		state = Connected;
 		Serial.println("connected to port");
+		//send ack after receiving frame
+		server->write(reinterpret_cast<const char*>(frameAck), FRAME_LEN);
 	}, nullptr);
 
 	client.onDisconnect([this](void*, AsyncClient* server){
 		//TODO - reconnect to wifi in case of total disconnect
 		state = Connecting;
+		if(camFrameBuf){
+			delete camFrameBuf;
+			camFrameBuf = nullptr;
+		}
 		Serial.println("disconnected from port");
 	});
 
 	client.onData([this](void* arg, AsyncClient* server, void* data, size_t len){
-		if(processData((uint8_t*)data, len)){
+
+		bool frameRead = processData((uint8_t*)data, len);
+
+		//send ack after receiving frame
+		server->write(reinterpret_cast<const char*>(frameAck), FRAME_LEN);
+
+		if(frameRead){
+			if(camFrame.jpgSize >= jpgMaxSize){
+				Serial.printf("jpgSize: %d, max size: %d\n", camFrame.jpgSize, jpgMaxSize);
+				Serial.println("insufficient buffer size");
+			}
 			func(camFrame);
 		}
 	}, nullptr);
 
 	client.onError([this](void*, AsyncClient* c, err_t error){
 		state = Connecting;
+		if(camFrameBuf){
+			delete camFrameBuf;
+			camFrameBuf = nullptr;
+		}
 		Serial.print("error occured: ");
 		Serial.println(c->errorToString(error));
 		connect();
@@ -39,6 +60,10 @@ bool FeedService::connect(){
 
 	client.onTimeout([this](void*, AsyncClient*, uint32_t time){
 		state = Connecting;
+		if(camFrameBuf){
+			delete camFrameBuf;
+			camFrameBuf = nullptr;
+		}
 		Serial.printf("timeout error %d passed\n", time);
 	}, nullptr);
 
@@ -61,6 +86,12 @@ void FeedService::onFrame(std::function<void(const CamFrame&)> func){
 
 void FeedService::disconnect(){
 	state = Idle;
+
+	if(camFrameBuf){
+		delete camFrameBuf;
+		camFrameBuf = nullptr;
+	}
+
 	client.close();
 }
 
@@ -82,7 +113,10 @@ bool FeedService::processData(const uint8_t* data, size_t len){
 
 		//when found, exit while and start filling buffers
 		if(frameStartMatch == FRAME_LEN){
-			camFrameBuf = new DataBuffer(sizeof(CamFrame) - sizeof(CamFrame::jpg) + jpgMaxSize);
+
+			if(camFrameBuf == nullptr){
+				camFrameBuf = new DataBuffer(sizeof(CamFrame) - sizeof(CamFrame::jpg) + jpgMaxSize);
+			}
 			inPacket = true;
 			frameStartMatch = 0;
 			frameEndMatch = 0;
@@ -134,7 +168,7 @@ bool FeedService::processData(const uint8_t* data, size_t len){
 		size_t restSize = sizeof(camFrame) - sizeof(camFrame.jpgSize) - sizeof(camFrame.jpg);
 		memcpy(&camFrame + sizeof(camFrame.jpgSize) + sizeof(camFrame.jpg), camFrameBuf->readData(), restSize);
 
-		delete camFrameBuf;
+		camFrameBuf->clear();
 
 		return true;
 	}
