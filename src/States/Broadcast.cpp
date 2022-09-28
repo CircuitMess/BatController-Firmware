3621310d6868bf1263ce3a654e5242a319f44ca8
subo <gamedevsubo@gmail.com>
2022-09-28 16:55:54 +0200

    Add server side TCP

--- src/States/Broadcast.cpp
+++ src/States/Broadcast.cpp
@@ -43,7 +43,41 @@ void Pair::Broadcast::onStart(){
 	Serial.print("\nAP IP address: ");
 	Serial.printf(IP.toString().c_str());
     Serial.printf("\nPass: %s", password);
-	server.begin();
+
+    server = new AsyncServer(63);
+
+    server->onClient([this](void* arg, AsyncClient* client){
+
+        Serial.println("new client connected");
+
+        client->onConnect([this](void*, AsyncClient* server){
+            Serial.println("client connected to port");
+        }, nullptr);
+
+        client->onDisconnect([this](void*, AsyncClient* server){
+            //TODO - reconnect to wifi in case of total disconnect
+            Serial.println("client disconnected from port");
+        });
+
+        client->onError([this](void*, AsyncClient* c, int8_t error){
+            Serial.print("error occured: ");
+            Serial.println(c->errorToString(error));
+        }, nullptr);
+
+        client->onData([this](void* arg, AsyncClient* server, void* data, size_t len){
+            Serial.printf("CLIENT DATA\n");
+            Serial.printf("%s", data);
+//            Serial.write((uint8_t *)data, len);
+        });
+
+        client->onTimeout([this](void*, AsyncClient*, uint32_t time){
+            Serial.printf("timeout error %d passed\n", time);
+        }, nullptr);
+
+    }, nullptr);
+
+
+	server->begin();
 
 	LoopManager::addListener(this);
 }
