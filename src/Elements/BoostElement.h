cc380a26cbec41e52d46a0b056609adcb89eaf07
Emil Gajšak <emil.gajsak@gmail.com>
2022-10-26 18:44:58 +0200

    Added BoostElement and necessary assets

---
+++ src/Elements/BoostElement.h
@@ -0,0 +1,33 @@
+#ifndef BATCONTROLLER_FIRMWARE_BOOSTELEMENT_H
+#define BATCONTROLLER_FIRMWARE_BOOSTELEMENT_H
+
+#include "../Interface/LVObject.h"
+
+class BoostElement : public LVObject {
+public:
+
+	explicit BoostElement(lv_obj_t* parent);
+/**
+	 * @param active True - on (fire), False - off (no fire)
+	 */
+	void setActive(bool active);
+	/**
+	 * @param level [0-100]
+	 */
+	void setLevel(uint8_t level);
+
+private:
+	lv_obj_t* bar;
+	lv_obj_t* bgGIF;
+
+	static constexpr uint8_t w = 27;
+	static constexpr uint8_t h = 118;
+
+	const char* fullPath = "S:/DriveScreen/Boost_full.bin";
+	const char* emptyPath = "S:/DriveScreen/Boost_empty.bin";
+	const char* activePath = "S:/DriveScreen/Boost_fire.gif";
+	const char* fillSegmentPath = "S:/DriveScreen/Boost_segment.bin";
+};
+
+
+#endif //BATCONTROLLER_FIRMWARE_BOOSTELEMENT_H
