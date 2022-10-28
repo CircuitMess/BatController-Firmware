f3ca5d23305a60c81140eccad90e223b2f1d9bc4
Emil Gajšak <emil.gajsak@gmail.com>
2022-10-28 16:52:09 +0200

    Added DriveScreen

---
+++ src/Screens/DriveScreen.cpp
@@ -0,0 +1,64 @@
+#include "DriveScreen.h"
+#include <Input/Input.h>
+#include <BatController.h>
+#include <DriveMode.h>
+#include "../Driver/ManualDriver.h"
+
+DriveScreen::DriveScreen(DriveMode mode) : infoElement(obj, mode){
+	topLayer = lv_obj_create(obj);
+	lv_obj_set_size(topLayer, lv_obj_get_width(obj), lv_obj_get_height(obj));
+	lv_obj_move_foreground(topLayer);
+
+	canvas = lv_canvas_create(obj);
+	lv_canvas_set_buffer(canvas, canvasBuf.data(), 160, 120, LV_IMG_CF_RAW);
+	lv_obj_set_pos(canvas, 0, 4);
+	lv_obj_move_background(canvas);
+
+
+	switch(mode){
+		case Idle:
+			break;
+		case Manual:
+			driver = std::make_unique<ManualDriver>(feed, topLayer);
+			break;
+		case Ball:
+			break;
+		case Line:
+			break;
+		case Marker:
+			break;
+	}
+
+	//TODO - feed.onFrame( draw na canvas)
+
+}
+
+void DriveScreen::onStart(){
+	driver->start();
+	Input::getInstance()->addListener(this);
+	//TODO - regg na status callback
+}
+
+void DriveScreen::onStop(){
+	driver->stop();
+	Input::getInstance()->removeListener(this);
+}
+
+void DriveScreen::buttonPressed(uint i){
+	switch(i){
+		case BTN_B:
+			//TODO - S3.honk, S3.blinkHeadlights, manual control override
+			break;
+		case BTN_MENU:
+			pop();
+			break;
+	}
+}
+
+void DriveScreen::buttonReleased(uint i){
+
+}
+
+void DriveScreen::loop(uint micros){
+
+}
---
+++ src/Screens/DriveScreen.h
@@ -0,0 +1,43 @@
+#ifndef BATCONTROLLER_FIRMWARE_DRIVESCREEN_H
+#define BATCONTROLLER_FIRMWARE_DRIVESCREEN_H
+
+#include "../Driver/Driver.h"
+#include "../Interface/LVScreen.h"
+#include <memory>
+#include <Input/InputListener.h>
+#include <DriveMode.h>
+#include <Loop/LoopListener.h>
+#include <array>
+#include <Display/Color.h>
+#include "../Elements/GeneralInfoElement.h"
+
+class Feed {
+
+};
+
+
+class DriveScreen : public LVScreen, private InputListener, private LoopListener {
+public:
+	DriveScreen(DriveMode mode);
+	void onStart() override;
+	void onStop() override;
+
+private:
+	void buttonPressed(uint i) override;
+	void buttonReleased(uint i) override;
+
+	void loop(uint micros) override;
+
+	Feed feed;
+	//FeedDisplay
+	std::unique_ptr<Driver> driver;
+	lv_obj_t* topLayer;
+
+	lv_obj_t* canvas;
+	std::array<Color, 160 * 120> canvasBuf;
+
+	GeneralInfoElement infoElement;
+};
+
+
+#endif //BATCONTROLLER_FIRMWARE_DRIVESCREEN_H
