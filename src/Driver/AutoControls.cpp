7505fa69764e34506e5f11c916f90f923b598706
Emil Gajšak <emil.gajsak@gmail.com>
2022-11-09 17:07:44 +0100

    Added AutoControls

---
+++ src/Driver/AutoControls.cpp
@@ -0,0 +1,40 @@
+#include "AutoControls.h"
+#include <BatController.h>
+#include <Com/Communication.h>
+
+//TODO - change speedModal 3rd argument to previously stored speed from settings
+AutoControls::AutoControls(lv_obj_t* container, LVScreen* parentScreen) : dirElement(container), speedModal(parentScreen, [](uint8_t speed){
+	//TODO - store changed speed to settings, apply to other components
+}, 0){
+	lv_obj_update_layout(container);
+	auto dirElementX = lv_obj_get_width(container) - lv_obj_get_width(dirElement.getLvObj()) - 3;
+	auto dirElementY = lv_obj_get_height(container) - lv_obj_get_height(dirElement.getLvObj()) - 3;
+	lv_obj_set_pos(dirElement.getLvObj(), dirElementX, dirElementY);
+}
+
+void AutoControls::start(){
+	BatController.getInput()->addListener(this);
+}
+
+void AutoControls::stop(){
+	BatController.getInput()->removeListener(this);
+	if(speedModal.isActive()) speedModal.stop();
+}
+
+void AutoControls::buttonPressed(uint i){
+	if(i == BTN_B){
+		Com.sendHonk();
+	}else if(i == BTN_UP || i == BTN_DOWN){
+		if(!speedModal.isActive()) speedModal.start();
+	}
+}
+
+void AutoControls::setSpeed(uint8_t speed){
+	dirElement.setSpeed(speed);
+}
+
+void AutoControls::setDirection(float angle){
+	dirElement.setDirection(angle);
+}
+
+
