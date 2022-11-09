21ac60024897fe6a95809aa85545b17a6bb0a50b
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2022-11-09 13:19:32 +0100

    DriveDirectionElement (#9)
    * Add arrow pics to data folder
    
    * Add DriveDirectionElement
    
    * Free all the allocated memory
    
    * Change the malloc type of buffers from Color to lv_color_t
    
    * Add starting values to angle and speed, and check their values in setters
    
    * Set starting values in constructor
    
    * Allocate buffer on stack and not on heap
    
    * Clamp the speed in the setter

---
+++ src/Elements/DriveDirectionElement.h
@@ -0,0 +1,39 @@
+#ifndef BATCONTROLLER_FIRMWARE_DRIVEDIRECTIONELEMENT_H
+#define BATCONTROLLER_FIRMWARE_DRIVEDIRECTIONELEMENT_H
+
+#include "../Interface/LVObject.h"
+
+class DriveDirectionElement : public LVObject {
+public:
+	DriveDirectionElement(lv_obj_t* parent);
+	~DriveDirectionElement();
+	/**
+	 * @param speed [0-100]
+	 */
+	void setSpeed(uint8_t speed);
+	/**
+	 * @param angle [0-360]
+	 * it could be any number, positive or negative
+	 * or out of the range, the code will set it
+	 * in range
+	 */
+	void setDirection(float angle);
+
+private:
+	void drawFill();
+	void drawRotation();
+
+	float angle;
+	uint8_t speed;
+
+	lv_obj_t* canvasMain;
+	lv_obj_t* canvasHelper;
+
+	lv_color_t* bufferMain;
+	lv_color_t* bufferHelper;
+
+	static constexpr const char* arrowEmptyPath = "/DriveDir/Arrow0.raw";
+	static constexpr const char* arrowFullPath = "/DriveDir/Arrow1.raw";
+};
+
+#endif //BATCONTROLLER_FIRMWARE_DRIVEDIRECTIONELEMENT_H
