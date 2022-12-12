a4b26de42dec0c2a898e9f41dc8ecfb38b56a6b9
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2022-12-12 16:55:58 +0100

    Add SettingsScreen (#30)
    * Add SettingsScreen
    
    * Add a TODO; screen switching needs to be done
    
    * Settings: scroll to top when first setting gets focused
    
    * Settings: remove ledcDetach from factory reset
    
    * Settings: rework popping
    
    * Add SettingsScreen starting to MainMenu
    
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

---
+++ src/Screens/SettingsScreen.h
@@ -0,0 +1,48 @@
+#ifndef BATCONTROLLER_FIRMWARE_SETTINGSSCREEN_H
+#define BATCONTROLLER_FIRMWARE_SETTINGSSCREEN_H
+
+#include "../Interface/LVScreen.h"
+#include <Input/InputListener.h>
+
+class SettingsScreen : public LVScreen, private InputListener {
+public:
+	SettingsScreen();
+
+	virtual ~SettingsScreen();
+
+	void onStarting() override;
+
+	void onStop() override;
+	void onStart() override;
+
+private:
+	lv_obj_t* sound;
+	lv_obj_t* shutdownTime;
+	lv_obj_t* screenBrightness;
+	lv_obj_t* factoryReset;
+	lv_obj_t* HWTest;
+	lv_obj_t* version;
+	lv_obj_t* pairBatmobile;
+
+	lv_obj_t* soundSlider;
+	lv_obj_t* shutdownTimeLabel;
+	lv_obj_t* shutdownSlider;
+	lv_obj_t* brightnessSlider;
+
+	lv_style_t style_knob;
+	lv_style_t style_def;
+	lv_style_t style_focused;
+	lv_style_t style_main;
+	lv_style_t style_pressed;
+
+	bool heldThresh = false;
+	void buttonPressed(uint i) override;
+	void buttonHeldRepeat(uint i, uint repeatCount) override;
+	void buttonHeld(uint i) override;
+	void buttonReleased(uint i) override;
+
+	void backToMain();
+
+};
+
+#endif //BATCONTROLLER_FIRMWARE_SETTINGSSCREEN_H
