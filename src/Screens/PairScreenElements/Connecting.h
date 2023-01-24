9ba4efab218d3668bd55e7b5dfd5b691b61ad053
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2023-01-24 15:39:10 +0100

    Local wi fi pair screens (#36)
    * Add InputPS
    
    * Add InputPS to PairScreen
    
    * Remove buffer and unused lv objects from PairScreen
    
    * Add  keyboard styles to BatTheme
    
    * BatThemeInit(disp)
    
    * Add ScanAruco
    
    * Add Error element for Pair Screen
    
    * Add Connecting element for Pair Screen
    
    * Add ScanQR element for Pair Screen
    
    * Add inputGroup and callback function to ScanAruco
    
    * Add inputGroup and callback functions to InputPS
    
    * Remove popovers in InputPS
    
    * Add setters for callbacks in InputPS
    
    * Add title to the InputPS
    
    * Add switching between text areas in InputPS
    
    * Add getters and setters for password and network in InputPS
    
    * Add Close button to keyboard in InputPS
    
    * Add event callbacks on start and remove the on stop in InputPS
    
    * Add minor changes to lv_obj_t input in InputPS
    
    * Add minor changes to lv_obj_t taNetwork in InputPS
    
    * Add minor changes to lv_obj_t taPassword in InputPS
    
    * Add minor changes to keyboard in InputPS
    
    * Add Deconstruct to InputPS
    
    * Add style to Text area in BatTheme
    
    * change styles->pressed in BatTheme
    
    * Add colors and other minor color related changes in BatTheme
    
    * Add changes to styles that are added to keyboard class in BatTheme
    
    * Add ssid and pass as parameters to callbackDone function in InputPS
    
    * Remove getters for network and password, as well as some empty spaces
    
    * Set the maximum number of characters for text areas to 21
    
    * Initialize PairScreen elements in the Constructor
    
    * Add Loop and loop related variables, for connecting timeout
    
    * Add minor changes to the PairScreen
    
    * Add WiFiService
    
    * Add WiFiConnecting.gif
    
    * Add WiFiOff.bin
    
    * Remove buffer from scanQR
    
    * Remove canvas from scanQR
    
    * Add lv_obj_t* qr for drawing QR code
    
    * Add ssid, password and ipAddress as parameters to start function in ScanQR
    
    * Add char* data and use it to draw QR code
    
    * Corrected formatting, minor syntax errors
    
    * Tweaked Pairing scan screen
    
    * Changed QR scan UI
    
    * Changed pair QR to contain whole WiFi details
    
    * Adjusted PairService and PairScreen to work with external wifi networks
    
    * Removed leftover debug output
    
    * Added WiFiService stop in PairService
    
    * Removed debug pairID setting
    
    * Adjust ScanQR layout and data
    
    * ScanQR and ScanAruco screen brightness
    
    * Move local wifi settings saving
    
    * Adjust InputPS limit
    
    * Add canvas clearing to ScanAruco
    
    * Reset random ID and wifi info each time PairScreen starts aruco mode
    
    * Set controllerIP on PairService starting
    
    Co-authored-by: Emil Gajšak <emil.gajsak@gmail.com>
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

---
+++ src/Screens/PairScreenElements/Connecting.h
@@ -0,0 +1,23 @@
+#ifndef BATCONTROLLER_FIRMWARE_CONNECTING_H
+#define BATCONTROLLER_FIRMWARE_CONNECTING_H
+
+
+#include <core/lv_obj.h>
+
+class Connecting {
+public:
+	Connecting(lv_obj_t* obj);
+	~Connecting();
+
+	void start();
+	void stop();
+
+private:
+	lv_obj_t* connecting;
+	lv_obj_t* gif;
+	lv_obj_t* text;
+
+};
+
+
+#endif //BATCONTROLLER_FIRMWARE_CONNECTING_H
