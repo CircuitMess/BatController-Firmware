aeb099ba708615e6a37cef2463b74af9a1e001b8
Filip Budiša <filip@circuitmess.com>
2023-01-27 11:53:19 +0100

    Add ball position drawing to BallDriver

--- src/Driver/BallDriver.cpp
+++ src/Driver/BallDriver.cpp
@@ -16,8 +16,23 @@ BallDriver::~BallDriver() {
 
 void BallDriver::onFrame(const DriveInfo& frame, Color* pixels){
 	if(frame.mode != DriveMode::Ball) return;
-
 	autoControls.setDirection(frame.motors);
+
+	if(frame.toBall() == nullptr || frame.toBall()->balls.empty()) return;
+	const auto& ball = frame.toBall()->balls.front();
+
+	lv_obj_t* canvas = lv_canvas_create(nullptr);
+	lv_canvas_set_buffer(canvas, pixels, 160, 120, LV_IMG_CF_TRUE_COLOR);
+
+	lv_draw_rect_dsc_t draw = {};
+	draw.border_width = 1;
+	draw.border_color = lv_color_make(255, 0, 0);
+	draw.border_opa = LV_OPA_COVER;
+	draw.border_side = LV_BORDER_SIDE_FULL;
+	draw.radius = LV_RADIUS_CIRCLE;
+	lv_canvas_draw_rect(canvas, ball.pos.x - ball.r, ball.pos.y - ball.r, ball.r*2, ball.r*2, &draw);
+
+	lv_obj_del(canvas);
 }
 
 void BallDriver::buttonPressed(uint i){
