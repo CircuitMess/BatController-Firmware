ed724eb0395a5cf6b1f6fb162526de8f48c7570f
Filip Budiša <filip@circuitmess.com>
2023-01-27 14:16:47 +0100

    Fix ball drawing

--- src/Driver/BallDriver.cpp
+++ src/Driver/BallDriver.cpp
@@ -25,10 +25,12 @@ void BallDriver::onFrame(const DriveInfo& frame, Color* pixels){
 	lv_canvas_set_buffer(canvas, pixels, 160, 120, LV_IMG_CF_TRUE_COLOR);
 
 	lv_draw_rect_dsc_t draw = {};
+	lv_draw_rect_dsc_init(&draw);
 	draw.border_width = 1;
 	draw.border_color = lv_color_make(255, 0, 0);
 	draw.border_opa = LV_OPA_COVER;
 	draw.border_side = LV_BORDER_SIDE_FULL;
+	draw.bg_opa = LV_OPA_0;
 	draw.radius = LV_RADIUS_CIRCLE;
 	lv_canvas_draw_rect(canvas, ball.pos.x - ball.r, ball.pos.y - ball.r, ball.r*2, ball.r*2, &draw);
 
