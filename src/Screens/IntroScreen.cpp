8a32cabeb836680d6bf96804c1e69194e96be714
Filip Budiša <filip@circuitmess.com>
2023-01-31 13:57:17 +0100

    Add screen brightness fade in to IntroScreen

--- src/Screens/IntroScreen.cpp
+++ src/Screens/IntroScreen.cpp
@@ -1,6 +1,8 @@
 #include "IntroScreen.h"
 #include "../Screens/PairScreen.h"
 #include <Loop/LoopManager.h>
+#include <Settings.h>
+#include <BatController.h>
 
 static const uint16_t FrameDurations[] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
                                           100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
@@ -14,6 +16,7 @@ IntroScreen::IntroScreen(void (* callback)()) : callback(callback){
 void IntroScreen::onStart(){
 	frameTime = millis();
 	LoopManager::addListener(this);
+	fade = 0;
 }
 
 void IntroScreen::onStop(){
@@ -21,6 +24,28 @@ void IntroScreen::onStop(){
 }
 
 void IntroScreen::loop(uint micros){
+	if(fade == 0){
+		fade = 1;
+		ledcSetup(6, 5000, 8);
+		ledcWrite(6, 255);
+		ledcAttachPin(PIN_BL, 6);
+		return;
+	}else if(fade < FadeTime){
+		fade = min(fade + micros, (uint32_t) FadeTime);
+		float t = (float) fade / (float) FadeTime;
+
+		uint8_t maxBrightness = map(Settings.get().screenBrightness, 0, 255, 240, 0);
+		uint8_t diff = (255 - maxBrightness);
+		uint8_t current = 255.0f - (float) diff * t;
+
+
+		ledcWrite(6, current);
+		printf("PWM %d\n", current);
+	}else if(!blInited){
+		blInited = true;
+		BatController.setBrightness(Settings.get().screenBrightness);
+	}
+
 	uint32_t time = millis();
 	if(time - frameTime < FrameDurations[currentFrame]) return;
 
