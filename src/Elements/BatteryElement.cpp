ba946ab343f1b4f974d67b6b3fe6dd5ace234433
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2023-02-02 19:09:40 +0100

    Low battery indicator bat 121 (#45)
    * Change checkInterval from 400000 to 500000
    
    * Add bool-s for blinking state
    
    * Add setBlinking to BatteryElement
    
    * Use '8' as an indicator for blinking state for controller battery
    
    * Change percentages in onBattery()
    
    * Fix indentation
    
    * Refactor GeneralInfoElement
    
    * Refactor BatteryElement blinking and charging
    
    * GeneralInfoElement: stop blinking when charging starts
    
    ---------
    
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

--- src/Elements/BatteryElement.cpp
+++ src/Elements/BatteryElement.cpp
@@ -19,32 +19,76 @@ BatteryElement::BatteryElement(lv_obj_t* parent, BatType type) : LVObject(parent
 	lv_img_set_src(device, imgPath);
 }
 
+BatteryElement::~BatteryElement(){
+	LoopManager::removeListener(this);
+}
+
 void BatteryElement::setLevel(uint8_t index){
 	char imgPath[50];
 	sprintf(imgPath, "S:/Battery/%d.bin", index);
 	lv_img_set_src(img, imgPath);
+	picIndex = index;
 }
 
 void BatteryElement::setCharging(bool charging){
 	if(this->charging == charging) return; // we don't want to add the same listener multiple times
 	this->charging = charging;
+
 	if(charging){
+		chargeTimer = 0;
+	}
+
+	if(blinking || charging){
 		LoopManager::addListener(this);
 	}else{
 		LoopManager::removeListener(this);
 	}
 }
 
-void BatteryElement::loop(uint micros){
-	microCounter += micros;
-	if(microCounter >= checkInterval){
-		microCounter = 0;
-		setLevel(picIndex);
-		picIndex++;
-		if(picIndex == 8) picIndex = 0;
+void BatteryElement::setBlinking(bool blinking){
+	if(this->blinking == blinking) return;
+	this->blinking = blinking;
+
+	if(blinking){
+		hidden = false;
+		blinkTimer = 0;
+	}else{
+		lv_obj_clear_flag(img, LV_OBJ_FLAG_HIDDEN);
+	}
+
+	if(blinking || charging){
+		LoopManager::addListener(this);
+	}else{
+		LoopManager::removeListener(this);
 	}
 }
 
-BatteryElement::~BatteryElement(){
-	setCharging(false);
+
+void BatteryElement::loop(uint micros){
+	if(!charging && !blinking){
+		LoopManager::removeListener(this);
+	}
+
+	if(blinking){
+		blinkTimer += micros;
+		if(blinkTimer >= BlinkInterval * 1000){
+			blinkTimer = 0;
+			hidden = !hidden;
+
+			if(hidden){
+				lv_obj_add_flag(img, LV_OBJ_FLAG_HIDDEN);
+			}else{
+				lv_obj_clear_flag(img, LV_OBJ_FLAG_HIDDEN);
+			}
+		}
+	}
+
+	if(charging){
+		chargeTimer += micros;
+		if(chargeTimer >= ChargeInterval){
+			chargeTimer = 0;
+
+			setLevel((picIndex + 1) % 8);
+		}
+	}
 }
