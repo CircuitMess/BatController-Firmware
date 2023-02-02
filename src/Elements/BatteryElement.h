f5ea91b30c756cf1b724608e97c0206f3f693eec
Filip Budiša <filip@circuitmess.com>
2023-02-02 19:06:04 +0100

    Refactor BatteryElement blinking and charging

--- src/Elements/BatteryElement.h
+++ src/Elements/BatteryElement.h
@@ -29,10 +29,13 @@ private:
 
 	bool blinking = false;
 	bool hidden = false;
+	uint32_t blinkTimer = 0;
+	static constexpr uint32_t BlinkInterval = 500; // [ms]
+
 	bool charging = false;
 	uint8_t picIndex = 0;
-	uint32_t microCounter = 0;
-	static const uint32_t checkInterval = 500000;
+	uint32_t chargeTimer = 0;
+	static const uint32_t ChargeInterval = 500000;
 };
 
 
