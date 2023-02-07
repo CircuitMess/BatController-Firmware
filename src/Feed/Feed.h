ad042dc110b165e293043f9bbd1a7b3b8abdd7d7
Filip Budiša <filip@circuitmess.com>
2023-02-07 10:09:58 +0100

    Feed: set JPG max size to 8k, RX buf to hold 2 frames

--- src/Feed/Feed.h
+++ src/Feed/Feed.h
@@ -47,8 +47,8 @@ private:
 	Task decodeTask;
 	void decodeFunc();
 
-	constexpr static size_t JpgMaxSize = 4500; //upper size limit for JPG quality 30 on Batmobile camera, approximately
-	constexpr static size_t RxBufSize = 3 * (sizeof(DriveInfo) + JpgMaxSize);
+	constexpr static size_t JpgMaxSize = 8000; //upper size limit for JPG quality 30 on Batmobile camera, approximately
+	constexpr static size_t RxBufSize = 2 * (sizeof(DriveInfo) + JpgMaxSize);
 
 	std::function<void(std::shared_ptr<const DriveInfo>, const Color* frame)> frameCallback;
 	std::function<void(const DriveInfo&, Color* frame)> postProcCallback;
