5acba960334d3b6566a7a0a45e7fa3e31c102562
Emil Gajšak <emil.gajsak@gmail.com>
2023-02-09 19:30:45 +0100

    Added Bats animation in MarkerDriver

---
+++ src/Elements/BatsZoomElement.h
@@ -0,0 +1,24 @@
+#ifndef BATCONTROLLER_FIRMWARE_BATSZOOMELEMENT_H
+#define BATCONTROLLER_FIRMWARE_BATSZOOMELEMENT_H
+
+
+#include "../Interface/LVObject.h"
+
+class BatsZoomElement : public LVObject {
+public:
+	explicit BatsZoomElement(lv_obj_t* parent);
+	~BatsZoomElement() override;
+
+	void startAnim();
+private:
+	static constexpr uint8_t maxScale = 5;
+	static constexpr uint8_t w = 9 * (maxScale + 1) + 1;
+	static constexpr uint8_t h = 5 * (maxScale + 1) + 1;
+	static constexpr uint32_t duration = 3000;
+	lv_obj_t* img;
+	lv_anim_t zoomAnim;
+	lv_timer_t* timer;
+};
+
+
+#endif //BATCONTROLLER_FIRMWARE_BATSZOOMELEMENT_H
