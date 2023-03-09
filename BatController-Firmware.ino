3bf3b7cb8f73006369fdfeff9cf18fcd0621490e
Emil Gajšak <emil.gajsak@gmail.com>
2023-03-09 15:03:23 +0100

    Added JigHWTest with SPIFFS checksum

--- BatController-Firmware.ino
+++ BatController-Firmware.ino
@@ -15,10 +15,37 @@
 #include "src/Screens/IntroScreen.h"
 #include "src/ShutdownService.h"
 #include "src/LowBatteryService.h"
+#include "src/JigHWTest/JigHWTest.h"
 
 Display* display;
 lv_disp_draw_buf_t drawBuffer;
-static uint8_t drawData[160*20*2];
+static uint8_t drawData[160 * 20 * 2];
+
+bool checkJig(){
+	char buf[7];
+	int wp = 0;
+
+	uint32_t start = millis();
+	while(millis() - start < 1000){
+		while(Serial.available()){
+			buf[wp] = Serial.read();
+			wp = (wp + 1) % 7;
+
+			for(int i = 0; i < 7; i++){
+				int match = 0;
+				static const char* target = "JIGTEST";
+
+				for(int j = 0; j < 7; j++){
+					match += buf[(i + j) % 7] == target[j];
+				}
+
+				if(match == 7) return true;
+			}
+		}
+	}
+
+	return false;
+}
 
 void lvglFlush(lv_disp_drv_t* disp, const lv_area_t* area, lv_color_t* color_p){
 	uint32_t w = (area->x2 - area->x1 + 1);
@@ -36,17 +63,25 @@ void setup(){
 	Serial.begin(115200);
 	BatController.begin(false);
 
+	if(checkJig()){
+		BatController.fadeIn(0);
+		printf("Jig\n");
+		auto test = new JigHWTest(display);
+		test->start();
+		for(;;);
+	}
+
 	display = BatController.getDisplay();
 	auto sprite = display->getBaseSprite();
 	sprite->resize(1, 1);
 
 	pinMode(PIN_BATT, INPUT);
-	srand(analogRead(PIN_BATT)*7+analogRead(PIN_BATT)*13);
+	srand(analogRead(PIN_BATT) * 7 + analogRead(PIN_BATT) * 13);
 
 	LoopManager::reserve(26);
 
 	lv_init();
-	lv_disp_draw_buf_init(&drawBuffer, drawData, nullptr, sizeof(drawData)/2);
+	lv_disp_draw_buf_init(&drawBuffer, drawData, nullptr, sizeof(drawData) / 2);
 
 	static lv_disp_drv_t displayDriver;
 	lv_disp_drv_init(&displayDriver);
@@ -54,7 +89,7 @@ void setup(){
 	displayDriver.ver_res = 128;
 	displayDriver.flush_cb = lvglFlush;
 	displayDriver.draw_buf = &drawBuffer;
-	lv_disp_t * disp = lv_disp_drv_register(&displayDriver);
+	lv_disp_t* disp = lv_disp_drv_register(&displayDriver);
 	BatThemeInit(disp);
 
 	if(Battery.getPercentage() < 1 && !Battery.charging()){
